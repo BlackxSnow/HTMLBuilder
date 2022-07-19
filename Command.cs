66bb73b88aa146ffa2422866a6c78d1fd89b25aa
Calvin Soueid <calvin@soueid.io>
2022-07-19 15:21:36 +1000

    Add `help <command>` functionality

--- Command.cs
+++ Command.cs
@@ -68,7 +68,66 @@ public static class Commands
         }
     }
     
-    private static void PrintHelp(Arguments.Argument[] args)
+    private static void PrintOption(StringBuilder output, Command.Option option, string prefix = "\t")
+    {
+        output.Append($"{prefix}-{option.Short}, --{option.Long} ");
+        if (option.Args.Length > 0) output.Append($"{option.Args} ");
+        output.AppendLine($"{option.Description}");
+    }
+    
+    private static void PrintSubcommand(StringBuilder output, Command.Subcommand sub)
+    {
+        output.Append($"\t{sub.Name}");
+        if (sub.Args.Length > 0) output.Append($" {sub.Args}");
+        output.AppendLine($" [Options...]");
+        output.AppendLine($"\t\t{sub.Description}");
+        if (sub.Options.Length == 0) return;
+        
+        output.AppendLine("\t\tOptions:");
+        foreach (var option in sub.Options)
+        {
+            PrintOption(output, option, "\t\t\t");
+        }
+    }
+    
+    private static StringBuilder PrintCommand(Command command)
+    {
+        var output = new StringBuilder(200);
+        output.AppendLine($"Help ({command.Name}):");
+        output.AppendLine("Usage:");
+        output.Append($"\t{command.Name} ");
+        if (command.Subcommands.Length > 0)
+        {
+            for (var i = 0; i < command.Subcommands.Length; i++)
+            {
+                output.Append(command.Subcommands[i].Name);
+                if (i != command.Subcommands.Length - 1) output.Append("|");
+            }
+        }
+        else if (command.Args.Length > 0)
+        {
+            output.Append($" {command.Args}");
+        }
+        output.AppendLine(" [Options...]");
+        output.AppendLine(command.Description);
+
+        if (command.Options.Length > 0) output.AppendLine("Options:");
+        foreach (var option in command.Options)
+        {
+            PrintOption(output, option);
+        }
+
+        if (command.Subcommands.Length == 0) return output;
+        
+        output.AppendLine("Subcommands: ");
+        foreach (var sub in command.Subcommands)
+        {
+            PrintSubcommand(output, sub);
+        }
+        return output;
+    }
+
+    private static StringBuilder PrintAll()
     {
         StringBuilder output = new(10 + _Commands.Count * 50);
         output.AppendLine("Help:");
@@ -77,7 +136,22 @@ public static class Commands
         {
             output.AppendLine($"\t{command.Key}: {command.Value.Description}");
         }
-        Console.WriteLine(output.ToString());
+
+        return output;
+    }
+    
+    private static void PrintHelp(Arguments.Argument[] args)
+    {
+        if (args.Length > 0)
+        {
+            Console.WriteLine(Commands.TryGet(args[0].Value, out var command)
+                ? PrintCommand(command).ToString()
+                : $"Unknown command '{args[0].Value}'");
+        }
+        else
+        {
+            Console.WriteLine(PrintAll().ToString());
+        }
     }
     
     private static readonly Command _Help = new Command()
