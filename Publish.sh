fe2d4f5ba13912fa21793aabb20b8323230f11bf
Calvin Soueid <calvin@soueid.io>
2022-07-19 12:36:15 +1000

    Add platform select to publish script

--- Publish.sh
+++ Publish.sh
@@ -1,1 +1,9 @@
-dotnet publish HTMLBuilder.sln -p:PublishSingleFile=true -r win-x64 -c Release --self-contained false
+read -p "Select target (1 - Win64, 2 - Linux64) " choice
+platform=""
+case $choice in
+    1 ) platform="win-x64" ;;
+    2 ) platform="linux-x64" ;;
+    * ) echo "Invalid input. " ;;
+esac
+
+dotnet publish HTMLBuilder.sln -p:PublishSingleFile=true -r $platform -c Release --self-contained false
